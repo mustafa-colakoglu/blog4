c7f64de182bf4e8ff4acdd109baa26f2c8ca1012
mustafa220 <musto_220@windowslive.com>
2015-08-08 12:37:53 +0300

    Aci uyumlama

--- blog4/main/controllers/admin/AdminController.php
+++ blog4/main/controllers/admin/AdminController.php
@@ -2,11 +2,38 @@
 	namespace Controllers\admin;
 	use MS\MSController;
 	use MSGet;
+	use MS\Acl;
 	class Admin extends MSController{
 		function __construct(){
 			parent::__construct();
+			$this->acl = new Acl();
+			$this->acl->setAccess(
+				array(
+					array(
+						"actions" => array("actionDeneme"),
+						"users" => array(@$_SESSION["blogGiris"],true),
+						"redirect" => "admin/actionLogin",
+						"ip" => "::1"
+					),
+					array(
+						"actions" => array("actionLogin"),
+						"users" => array(@$_SESSION["blogGiris"],false),
+						"redirect" => "admin",
+						"ip" => "::1"
+					)
+				)
+			);
 		}
 		function actionIndex(){
+			$giris = @$_SESSION["blogGiris"];
+			if(!$giris){
+				$this->actionLogin();
+			}
+			else{
+				$this->actionDeneme();
+			}
+		}
+		function actionLogin(){
 			$giris = @$_SESSION["blogGiris"];
 			if($_POST and !$giris){
 				if(@$_POST["kadi"] === "mustafa220" and @$_POST["sifre"] === "mustafa"){
@@ -14,14 +41,15 @@
 					$giris = true;
 				}
 			}
-			if(!$giris){
-				$this->view("admin/adminBas",array("title" => "Giriş Yapın !"));
-				$this->view("admin/girisPanel");
-				$this->view("admin/adminSon");
+			if($giris){
+				header("Location:".$this->site."/admin/actionDeneme");
 			}
-			else{
-				header("Location:".MSGet::getSite()."/admin/anasayfa");
-			}
+			$this->view("admin/adminBas",array("title" => "Giriş Yapın !"));
+			$this->view("admin/girisPanel");
+			$this->view("admin/adminSon");
+		}
+		function actionDeneme(){
+			echo "burasi Deneme action u bunu sadece giris yapanlar ve ip si ::1 olanlar gorebilir";
 		}
 	}
 ?>
